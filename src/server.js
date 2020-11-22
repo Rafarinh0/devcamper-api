432e08b0d655ea29d9961c625ac53eea792352ea
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-11-22 13:15:57 -0300

    Adição do Morgan

--- src/server.js
+++ src/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const routes = require('./routes');
+const morgan = require('morgan');
 
 //Carregando variaveis de desenvolvimento
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
-
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 //Montando rotador
-
 app.use('/api/v1/bootcamps', routes);
 
 const PORT = process.env.PORT || 5000;
