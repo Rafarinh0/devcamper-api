9491c5b6254682eb05327a6aab69af850ec162a6
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-11-23 17:21:58 -0300

    Conexão com mongoose e handler global

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
 const routes = require('./routes');
 const morgan = require('morgan');
+const connectDB = require('./config/database');
 
 //Carregando variaveis de desenvolvimento
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: '..config.env' });
+
+//Conexão c o banco de dados
+connectDB();
 
 const app = express();
 //Dev logging middleware
@@ -17,5 +20,11 @@ app.use('/api/v1/bootcamps', routes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port 
-${PORT}`))
+const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (error, promise) => {
+    console.log(`Error: ${error.message}`);
+    //Close server and exit proccess
+    server.close(() => process.exit(1));
+})
