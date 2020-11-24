4a60dc34bb75ad1068d2b1679ad095b91d3209fc
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-11-24 18:19:54 -0300

    Classe de ErrorResponse

--- src/controllers/bootcamps.js
+++ src/controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const { addListener } = require('../models/Bootcamp');
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
 
 // @desc    Get all bootcamps
 //@route    GET /api/v1/bootcamps
@@ -22,12 +23,12 @@ exports.getBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id)
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({ success: true, data: bootcamp })
     } catch (error) {
-        res.status(400).json({ success: false });
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
 };
 
