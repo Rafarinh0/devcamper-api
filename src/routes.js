d6fa828c04dc31edc194fcabc15bbdfa9792bbd9
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-11-26 20:12:21 -0300

    Get de bootcamps por distancia

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('./controllers/bootcamps');
+const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampInRadius } = require('./controllers/bootcamps');
 
 const router = express.Router();
+
+router.route('/:radius/:zipcode/:distance').get(getBootcampInRadius);
 
 router.route('/').get(getBootcamps).post(createBootcamp);
 
