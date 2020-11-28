f2f1017462eae013dfade7e03e9c5b7eafe50577
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-11-28 13:24:54 -0300

    Model e Seeder de cursos

--- src/seeder.js
+++ src/seeder.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 require('dotenv/config');
 
+//Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Courses');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -14,11 +16,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 //Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/data/bootcamps.json`, 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/data/courses.json`, 'utf-8'))
 
 //Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log('Data imported...');
         process.exit();
     } catch (error) {
@@ -30,6 +34,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Data Destroyed...');
         process.exit();
     } catch (error) {
