02b0b02b70a81a2e754a02acdb345bf26a295069
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-12-02 21:19:44 -0300

    Sign Get JWT

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -16,6 +16,8 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     });
 
+    //Create token
+    const token = user.getSignedJwtToken();
 
-    res.status(200).json({ success: true });
+    res.status(200).json({ success: true, token: token });
 });
