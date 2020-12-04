7f1c5266b06e2af5c7e5525daa53208a1fada20f
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-12-04 20:54:56 -0300

    Middleware protect

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -62,4 +62,16 @@ const sendTokenResponse = (user, statusCode, res) => {
     }
 
     res.status(statusCode).cookie('token', token, options).json({ success: true, token: token });
-}
+};
+
+// @desc    Get current logged in user
+//@route    GET /api/v1/auth/me
+//@access   Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,11 +5,13 @@ const advancedResults = require('../middlewares/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+const { protect } = require('../middlewares/auth');
+
 router.route('/').get(advancedResults(Course, {
     path: 'bootcamp',
     select: 'name description',
 }),
-    getCourses).post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+    getCourses).post(protect, addCourse);
+router.route('/:id').get(getCourse).put(protect, updateCourse).delete(protect, deleteCourse);
 
 module.exports = router;
