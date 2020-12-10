86f2d4868044f88032f74d6a29a632bd481f31da
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-12-10 00:27:52 -0300

    Relação bootcamp/usuário e limitação de bootcamps

--- src/seeder.js
+++ src/seeder.js
@@ -5,6 +5,7 @@ require('dotenv/config');
 //Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -16,13 +17,15 @@ mongoose.connect(process.env.MONGO_URI, {
 
 //Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/data/bootcamps.json`, 'utf-8'));
-const courses = JSON.parse(fs.readFileSync(`${__dirname}/data/courses.json`, 'utf-8'))
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/data/courses.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/data/users.json`, 'utf-8'));
 
 //Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
         console.log('Data imported...');
         process.exit();
     } catch (error) {
@@ -35,6 +38,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
         console.log('Data Destroyed...');
         process.exit();
     } catch (error) {
