a9ef6314163df73a24e56ceede7c1aa7903ef348
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-12-12 17:48:33 -0300

    update de dados do usuário

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
+const { register, login, getMe, forgotPassword, resetPassword, updateDetails } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
+router.put('/updatedetails', protect, updateDetails);
 
 module.exports = router;
