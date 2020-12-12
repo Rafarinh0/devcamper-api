a9a5f5ea86a60cb02dc47b550e58c16a031c878d
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-12-12 18:33:22 -0300

    update de senha

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword, updateDetails } = require('../controllers/auth');
+const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -8,8 +8,9 @@ const { protect } = require('../middlewares/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatepassword', protect, updatePassword);
+router.put('/updatedetails', protect, updateDetails);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
-router.put('/updatedetails', protect, updateDetails);
 
 module.exports = router;
