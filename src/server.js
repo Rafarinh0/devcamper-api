bdece727e305fd9a785185d836e85bb2c7ed3682
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-12-12 19:42:12 -0300

    CRUD de admnistrador

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
 
 app.use(errorHandler);
 
