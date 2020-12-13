c8ff0309e6a9ea5d6ac79582ab75586b80a1365f
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-12-13 18:08:54 -0300

    modelo, controller, router e index/show de reviews

--- src/routes/bootcamps.js
+++ src/routes/bootcamps.js
@@ -5,6 +5,7 @@ const Bootcamp = require('../models/Bootcamp');
 
 //Include other resource routers
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ const { protect, authorize } = require('../middlewares/auth');
 
 //Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/:radius/:zipcode/:distance').get(getBootcampInRadius);
 
